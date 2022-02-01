d7668460b9b89264bd893604272d9d6920cf872d
Heba-Webdev23 <jtvonline@yahoo.com>
2022-02-01 21:35:35 +0300

    made changes in css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo'
 
 function App() {
+
+  const [theme, setTheme] = React.useState('light');
+
   return (
-    <div className="app">
+    <div className="app" data-theme='theme'>
     <Todo />
     </div>
   );
--- src/Todo.js
+++ src/Todo.js
@@ -2,12 +2,20 @@ import React, {useState} from "react";
 import './Todo.css'
 import moon from './assets/icon-moon.svg'
 
+
 export default function Todo() {
+    const [theme, setTheme] = React.useState('light');
+    const switchTheTheme = () => {
+        
+            setTheme('dark')
+        
+        
+    }
     return (
-        <main className="todo-wrapper">
+        <main className="todo-wrapper" data-theme={theme}>
          <nav className="navbar">
              <h1>T O D O</h1>
-             <img src={moon} />
+             <img src={moon} onClick={switchTheTheme}/>
          </nav>
 
          <div className="todo-app">
